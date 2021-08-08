1b23c88fa4a76d24d6bf64ce32f764616c6173dc
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-08-08 18:30:34 +0700

    Исправление ошибок, подготовка к созданию миграций БД

--- EducationalApp.API/Startup.cs
+++ EducationalApp.API/Startup.cs
@@ -15,7 +15,7 @@ namespace EducationalApp.API
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; set; }
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -24,6 +24,7 @@ namespace EducationalApp.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>();
             services.AddMvc();
         }
 
