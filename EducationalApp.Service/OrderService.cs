c7f1464122fb54a7929b73141850924bd52521fb
Dmitry Pavlov <bettercalldmitry@outlook.com>
2021-08-10 16:34:10 +0700

    Примерная реализация сервиса с использованием Unit of Work

--- EducationalApp.Service/OrderService.cs
+++ EducationalApp.Service/OrderService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EducationalApp.Data.Infrastructure;
+using EducationalApp.Data.Repository;
+using EducationalApp.Model.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +11,59 @@ namespace EducationalApp.Service
 {
     public interface IOrderService
     {
-
+        IEnumerable<Order> GetOrders();
+        Order GetOrder(Guid id);
+        Order CreateOrder(Order order);
+        void UpdateOrder(Order order);
+        void DeleteOrder(Guid id);
+        void SaveOrder();
     }
     public class OrderService : IOrderService
     {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderService(IOrderRepository orderRepository, IUnitOfWork unitOfWork)
+        {
+            _orderRepository = orderRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Order CreateOrder(Order order)
+        {
+            _orderRepository.Insert(order);
+            SaveOrder();
+            return order;
+        }
+
+        public void DeleteOrder(Guid id)
+        {
+            _orderRepository.Delete(id);
+            SaveOrder();
+        }
+
+        public Order GetOrder(Guid id)
+        {
+            var order = _orderRepository.GetById(id);
+            return order;
+        }
+
+        public IEnumerable<Order> GetOrders()
+        {
+            var orders = _orderRepository.GetAll().OrderByDescending(d => d.CreatedAt);
+            return orders;
+
+        }
+
+        public void SaveOrder()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            _orderRepository.Update(order);
+            SaveOrder();
+        }
     }
 }
